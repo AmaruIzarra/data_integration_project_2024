481ada7160a5fe18d70608ed0b7c9ba117270d3d
aizarrajacome <amaru.izarra@gmail.com>
2024-06-22 14:34:50 -0400

    adding program to find convex hull of stations coordinates

---
+++ convex_hull.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 14:17:31 2024
+
+@author: amaru
+"""
+from functools import cmp_to_key
+
+"""
+This program was created to find the convex hull of a set of coordinates. It 
+implements the Graham Scan Algorithm which has a worst case time complexity 
+of O(n log(n)) where n is the number of coordinates. 
+"""
+
+
+
